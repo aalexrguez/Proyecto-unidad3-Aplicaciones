f91a46f27b69fa1e45c93efa37c35d2fb461d1ce
Alex Rodriguez <alejandro.rodriguez112m@gmail.com>
2025-03-30 18:00:09 -0600

    Se agrego la funcion de mostrar los contacto en el textView

--- app/src/main/java/mx/tecnm/chi2/listacontactos/ver_contactos.java
+++ app/src/main/java/mx/tecnm/chi2/listacontactos/ver_contactos.java
@@ -1,8 +1,11 @@
 package mx.tecnm.chi2.listacontactos;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +16,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class ver_contactos extends AppCompatActivity {
 
 Button button_regresar;
-
+AdminSQLlite admin;
+TextView textMultiLineContactos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +29,11 @@ Button button_regresar;
             return insets;
         });
         button_regresar = findViewById(R.id.button_volver);
+        textMultiLineContactos = findViewById(R.id.TextMultiLine_contactos);
+
+        admin = new AdminSQLlite(this,"administracion",null,1);
+
+        mostrarContactos();
 
         button_regresar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,4 +43,29 @@ Button button_regresar;
         });
 
     }// cierre de onCreate
+
+    public void mostrarContactos(){
+        SQLiteDatabase db = admin.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM Contacto",null);
+
+        StringBuilder contactos = new StringBuilder();
+
+        while (cursor.moveToNext()) {
+            int id = cursor.getInt(0);
+            String nombre = cursor.getString(1);
+            String telefono = cursor.getString(2);
+            String email = cursor.getString(3);
+
+            contactos.append("ID: ").append(id)
+                    .append("\nNombre: ").append(nombre)
+                    .append("\nTeléfono: ").append(telefono)
+                    .append("\nEmail: ").append(email)
+                    .append("\n----------------------\n");
+        }
+
+        cursor.close();
+        db.close();
+
+        textMultiLineContactos.setText(contactos.toString());
+    }
 }
